3c8c87f8ce3caf1e012ed6f00e36b66057696fd3
Janderson <jandersonrodrigues216@gmail.com>
2025-11-08 21:48:30 -0300

    new change

--- src/main/java/com/marketcontrol/Super/model/ProductModel.java
+++ src/main/java/com/marketcontrol/Super/model/ProductModel.java
@@ -1,17 +1,30 @@
 package com.marketcontrol.Super.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.UUID;
 
+
 @Data @Entity @Table(name = "products")
-public class ProductModel {
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID idProduct;
+    @Column(nullable = false)
     private String name;
-    private double codigo_barras;
+    @Column(name = "codigo_barras", unique = true)
+    private double cod_barras;
+    private long categoria_id;
 
 }
