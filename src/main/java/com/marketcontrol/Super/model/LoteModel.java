895850dfbadf31753f4f8838ea59db096610088b
Janderson <jandersonrodrigues216@gmail.com>
2025-11-20 13:28:28 -0300

    LoteModel adicionado

--- src/main/java/com/marketcontrol/Super/model/LoteModel.java
+++ src/main/java/com/marketcontrol/Super/model/LoteModel.java
@@ -1,4 +1,41 @@
 package com.marketcontrol.Super.model;
 
+import com.marketcontrol.Super.model.enums.StatusLote;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_lotes")
 public class LoteModel {
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idLote;
+
+    @Column(name = "data_validade", nullable = false)
+    private LocalDate expirationDate;
+
+    @Column(name = "qtd_inicial", nullable = false)
+    private Integer quantidadeInicial;
+
+    @Column(name = "qtd_atual", nullable = false)
+    private Integer quantidadeAtual;
+
+    @Column(name = "prc_custo")
+    private BigDecimal precoCusto;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusLote statusLote;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "produto_id", nullable = false)
+    private ProductModel produto;
 }
--- src/main/java/com/marketcontrol/Super/model/ProductModel.java
+++ src/main/java/com/marketcontrol/Super/model/ProductModel.java
@@ -3,28 +3,35 @@ package com.marketcontrol.Super.model;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 
 @Data @Entity @Table(name = "products")
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true) //  PARA COMPARAR OBJETOS PELO ID
 public class ProductModel implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
+    @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID idProduct;
     @Column(nullable = false)
     private String name;
     @Column(name = "codigo_barras", unique = true)
-    private double cod_barras;
+    private String cod_barras;
     private long categoria_id;
 
+    @OneToMany(mappedBy = "produto", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<LoteModel> lotes = new ArrayList<>();
 }
