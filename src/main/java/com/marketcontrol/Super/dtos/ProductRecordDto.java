fea007a74d426ed6e12a16591735a673787848b5
Janderson <jandersonrodrigues216@gmail.com>
2025-11-21 13:40:56 -0300

    valid adicionada ao adicionar um novo produto ao sistema

--- src/main/java/com/marketcontrol/Super/dtos/ProductRecordDto.java
+++ src/main/java/com/marketcontrol/Super/dtos/ProductRecordDto.java
@@ -3,5 +3,5 @@ package com.marketcontrol.Super.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record ProductRecordDto(@NotBlank String name, @NotBlank String cod_barras, @NotNull Long categoria_id) {
+public record ProductRecordDto(@NotBlank String name, @NotBlank String cod_barras, @NotNull String categoria) {
 }
