b54bb743d86776a120e0eb8b9ff1fa7d09fffd0e
Janderson <jandersonrodrigues216@gmail.com>
2025-11-21 13:42:44 -0300

    camada de servico criada para lidar com as regras de negocio

--- src/main/java/com/marketcontrol/Super/repositories/LoteRepository.java
+++ src/main/java/com/marketcontrol/Super/repositories/LoteRepository.java
@@ -1,4 +1,20 @@
 package com.marketcontrol.Super.repositories;
 
-public interface LoteRepository {
+import com.marketcontrol.Super.model.LoteModel;
+import com.marketcontrol.Super.model.enums.StatusLote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface LoteRepository  extends JpaRepository<LoteModel, UUID> {
+
+    List<LoteModel> findByDataValidadeBetweenAndStatus(LocalDate dataInicio, LocalDate dataFim, StatusLote status);
+
+    List<LoteModel> findByDataValidadeBeforeAndStatus(LocalDate hoje, StatusLote status);
+
+    List<LoteModel> findByProdutoId(UUID produtoId);
 }
