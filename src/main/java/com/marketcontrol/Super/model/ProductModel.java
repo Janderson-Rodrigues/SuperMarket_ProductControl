0b95d6de38d016bd90124b5e1e729807cfbd5416
Janderson <jandersonrodrigues216@gmail.com>
2025-11-21 13:43:43 -0300

    categoria modifile

--- src/main/java/com/marketcontrol/Super/model/ProductModel.java
+++ src/main/java/com/marketcontrol/Super/model/ProductModel.java
@@ -1,10 +1,8 @@
 package com.marketcontrol.Super.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -13,7 +11,8 @@ import java.util.List;
 import java.util.UUID;
 
 
-@Data @Entity
+@Data
+@Entity
 @Table(name = "products")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true) //  PARA COMPARAR OBJETOS PELO ID
 public class ProductModel implements Serializable {
@@ -25,11 +24,14 @@ public class ProductModel implements Serializable {
     @EqualsAndHashCode.Include
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID idProduct;
+
     @Column(nullable = false)
     private String name;
+
     @Column(name = "codigo_barras", unique = true)
     private String cod_barras;
-    private long categoria_id;
+
+    private String categoria;
 
     @OneToMany(mappedBy = "produto", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<LoteModel> lotes = new ArrayList<>();
