9907f17d6c98e3d7fd255f3b9d510248d6ce4ba5
Janderson <jandersonrodrigues216@gmail.com>
2025-11-22 11:33:31 -0300

    regras de negocio do produto

--- src/main/java/com/marketcontrol/Super/services/ProductService.java
+++ src/main/java/com/marketcontrol/Super/services/ProductService.java
@@ -1,7 +1,5 @@
 package com.marketcontrol.Super.services;
 
-
-
 import com.marketcontrol.Super.dtos.ProductRecordDto;
 import com.marketcontrol.Super.model.ProductModel;
 import com.marketcontrol.Super.repositories.ProductRepository;
@@ -20,34 +18,25 @@ public class ProductService {
     @Autowired
     ProductRepository productRepository;
 
-
     @Transactional
-    public ProductModel save(ProductRecordDto productRecordDto){
-        // REGRA 1: Não permitir duplicidade de código de barras
-        // (Você precisa criar o método existsByCodBarras no Repository para isso funcionar)
-        // if (productRepository.existsByCodBarras(dto.cod_barras())) {
-        //    throw new RuntimeException("Já existe um produto com este código de barras.");
-        // }
+    public ProductModel save(ProductRecordDto productRecordDto) {
         var productModel = new ProductModel();
-        // Mágica: Como os nomes são iguais (name, cod_barras, category),
-        // o BeanUtils copia TUDO sozinho. Não precisa setar categoria manualmente.
         BeanUtils.copyProperties(productRecordDto, productModel);
-        // REGRA 2: Padronização (Regra de Negócio)
-        // Vamos salvar a categoria sempre em MAIÚSCULO para evitar "limpeza" e "Limpeza"
         productModel.setCategoria(productModel.getCategoria().toUpperCase());
         productModel.setName(productModel.getName().toUpperCase());
 
         return productRepository.save(productModel);
     }
-    public List<ProductModel> findAll(){
+
+    public List<ProductModel> findAll() {
         return productRepository.findAll();
     }
 
-    public Optional<ProductModel> findByid(UUID id){
+    public Optional<ProductModel> findByid(UUID id) {
         return productRepository.findById(id);
     }
 
-    public ProductModel update(UUID id, ProductRecordDto productRecordDto){
+    public ProductModel update(UUID id, ProductRecordDto productRecordDto) {
         Optional<ProductModel> product0 = productRepository.findById(id);
         if (product0.isEmpty()) {
             throw new RuntimeException("Product not found");
@@ -59,8 +48,8 @@ public class ProductService {
         return productRepository.save(productModel);
     }
 
-    public void delete(UUID id){
-        if (!productRepository.existsById(id)){
+    public void delete(UUID id) {
+        if (!productRepository.existsById(id)) {
             throw new RuntimeException("Product not found");
         }
         productRepository.deleteById(id);
