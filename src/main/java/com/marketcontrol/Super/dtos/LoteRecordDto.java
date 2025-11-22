15e7a6c13497cbb4c4924eb8b012613eb386b798
Janderson <jandersonrodrigues216@gmail.com>
2025-11-22 11:36:51 -0300

    camada de validaçcao e conversao para json da classe LoteModel

--- src/main/java/com/marketcontrol/Super/dtos/LoteRecordDto.java
+++ src/main/java/com/marketcontrol/Super/dtos/LoteRecordDto.java
@@ -1,4 +1,11 @@
 package com.marketcontrol.Super.dtos;
 
-public record LoteRecordDto() {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record LoteRecordDto(@NotNull UUID idProduct, @NotNull LocalDate expirationDate, @NotNull @Positive Integer quantidade, BigDecimal precoCusto) {
 }
