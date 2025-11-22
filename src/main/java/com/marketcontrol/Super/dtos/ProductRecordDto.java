92991e7200c719b55e8ea15b8b999f36ae16f008
Janderson <jandersonrodrigues216@gmail.com>
2025-11-22 11:29:12 -0300

    camada de validação e conversao para json e objeto

--- src/main/java/com/marketcontrol/Super/dtos/ProductRecordDto.java
+++ src/main/java/com/marketcontrol/Super/dtos/ProductRecordDto.java
@@ -3,5 +3,5 @@ package com.marketcontrol.Super.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record ProductRecordDto(@NotBlank String name, @NotBlank String cod_barras, @NotNull String categoria) {
+public record ProductRecordDto(@NotBlank String name, @NotBlank String codBarras, @NotNull String categoria) {
 }
